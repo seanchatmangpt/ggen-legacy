348df5bb4d82e076109f51cc49ef4c8ce99f0c3a
Sean Chatman <136349053+seanchatmangpt@users.noreply.github.com>
2026-08-02 21:14:56 -0700

    feat(lsp): advertise complete received capability set

--- src/capabilities.rs
+++ src/capabilities.rs
@@ -5,17 +5,54 @@ pub fn server_capabilities() -> ServerCapabilities {
         text_document_sync: Some(TextDocumentSyncCapability::Kind(TextDocumentSyncKind::FULL)),
         completion_provider: Some(CompletionOptions {
             trigger_characters: Some(vec![
-                ":".into(),
-                "@".into(),
-                "{".into(),
-                "[".into(),
+                ":".into(), "@".into(), ".".into(), "{".into(), "[".into(),
+                "\"".into(), "|".into(),
             ]),
             ..Default::default()
         }),
         hover_provider: Some(HoverProviderCapability::Simple(true)),
+        definition_provider: Some(OneOf::Left(true)),
+        references_provider: Some(OneOf::Left(true)),
+        rename_provider: Some(OneOf::Right(RenameOptions {
+            prepare_provider: Some(true),
+            work_done_progress_options: WorkDoneProgressOptions::default(),
+        })),
         document_symbol_provider: Some(OneOf::Left(true)),
+        workspace_symbol_provider: Some(OneOf::Left(true)),
         document_formatting_provider: Some(OneOf::Left(true)),
-        code_action_provider: Some(CodeActionProviderCapability::Simple(true)),
+        document_range_formatting_provider: Some(OneOf::Left(true)),
+        code_action_provider: Some(CodeActionProviderCapability::Options(CodeActionOptions {
+            code_action_kinds: Some(vec![CodeActionKind::QUICKFIX]),
+            resolve_provider: Some(false),
+            work_done_progress_options: WorkDoneProgressOptions::default(),
+        })),
+        folding_range_provider: Some(FoldingRangeProviderCapability::Simple(true)),
+        semantic_tokens_provider: Some(
+            SemanticTokensServerCapabilities::SemanticTokensOptions(SemanticTokensOptions {
+                legend: SemanticTokensLegend {
+                    token_types: vec![
+                        SemanticTokenType::NAMESPACE,
+                        SemanticTokenType::CLASS,
+                        SemanticTokenType::PROPERTY,
+                        SemanticTokenType::VARIABLE,
+                        SemanticTokenType::KEYWORD,
+                        SemanticTokenType::STRING,
+                        SemanticTokenType::NUMBER,
+                        SemanticTokenType::COMMENT,
+                        SemanticTokenType::FUNCTION,
+                    ],
+                    token_modifiers: Vec::new(),
+                },
+                range: None,
+                full: Some(SemanticTokensFullOptions::Bool(true)),
+                ..Default::default()
+            }),
+        ),
+        inlay_hint_provider: Some(OneOf::Left(true)),
+        code_lens_provider: Some(CodeLensOptions {
+            resolve_provider: Some(false),
+        }),
+        call_hierarchy_provider: Some(CallHierarchyServerCapability::Simple(true)),
         ..Default::default()
     }
 }
